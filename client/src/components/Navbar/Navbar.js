bfcf0ff48c56cae6d60827cf775ae17901b02678
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-04-30 14:34:47 +0530

    Search Feature Implemented.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,6 +6,7 @@ import  decode from 'jwt-decode';
 
 import useStyles from './styles';
 import memories from '../../images/memories.png';
+import memoriesText from '../../images/memories-Text.png';
 
 const Navbar = () => {
     const classes = useStyles();
@@ -37,10 +38,10 @@ const Navbar = () => {
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-        <div className={classes.brandContainer}>
-            <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memorinetic</Typography>
-            <img className={classes.image} src={memories} alt="memories" height="60" />
-        </div>
+        <Link to="/" className={classes.brandContainer}>
+            <img src={memoriesText} alt="logo" height="45px" />
+            <img className={classes.image} src={memories} alt="icon" height="40px" />
+        </Link>
         <Toolbar className={classes.toolbar}>
             {user ? (
                 <div className={classes.profile}>
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,4 +1,4 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+import { FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 export default (posts = [], action) => {
     switch (action.type) {
         case DELETE:
@@ -7,6 +7,8 @@ export default (posts = [], action) => {
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case FETCH_ALL:
             return action.payload;
+        case FETCH_BY_SEARCH:
+            return action.payload;    
         case CREATE:
             return [...posts, action.payload];
         default:
