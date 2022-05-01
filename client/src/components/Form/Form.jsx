20eda1837f8e17fe7041c478a32d67f4a84008b4
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-05-01 20:21:20 +0530

    Recommended Posts Implemented.

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
@@ -11,6 +12,7 @@ const Form = ({ currentId, setCurrentId }) => {
     const post = useSelector((state) => currentId ? state.posts.posts.find((post) => post._id === currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
+    const history = useHistory();
     const user = JSON.parse(localStorage.getItem('profile'));
 
     useEffect(() => {
@@ -23,7 +25,7 @@ const Form = ({ currentId, setCurrentId }) => {
       if(currentId) {
         dispatch(updatePost(currentId, { ...postData, name: user?.result?.name } ));
       } else {
-        dispatch(createPost({ ...postData, name: user?.result?.name }));
+        dispatch(createPost({ ...postData, name: user?.result?.name }, history));
       }
       clear();
     };
--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -42,7 +42,14 @@ const Post = ({ post, setCurrentId }) => {
                 </div>
                 {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
                     <div className={classes.overlay2}>
-                        <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}>
+                        <Button 
+                            style={{ color: 'white' }} 
+                            size="small" 
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                setCurrentId(post._id)
+                            }}
+                        >
                             <MoreHorizIcon fontSize="medium" />
                         </Button>
                     </div>
